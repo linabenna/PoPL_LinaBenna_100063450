c27c55d6aa55be988c057aa273b52d1a79247517
linabenna <linabenna1@gmail.com>
2025-01-29 22:03:31 +0400

    Add files via upload

---
+++ 01_Variables/variables2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Problem 02
+    let x = 10; // we must initialize x first
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
