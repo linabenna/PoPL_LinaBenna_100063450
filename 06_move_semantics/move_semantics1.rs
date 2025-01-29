8c8c2f154eb5fc8497c54e4ca1159be99cec2e71
linabenna <linabenna1@gmail.com>
2025-01-29 22:22:34 +0400

    Add files via upload

--- 06_move_semantics/move_semantics1.rs
+++ 06_move_semantics/move_semantics1.rs
@@ -4,14 +4,13 @@ fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
 }
 
 fn main() {
-    // Experimenting with vec0 and vec1
     let vec0 = vec![1, 2, 3];
     println!("Original vec0: {:?}", vec0);
 
-    let vec1 = fill_vec(vec0.clone()); // Clone vec0 to keep it intact
+    let vec1 = fill_vec(vec0.clone());
     println!("Vec1 after fill_vec: {:?}", vec1);
 
-    // vec0 remains unchanged
+    // Experimenting with modifying the original vec0 after cloning it
     println!("Vec0 after cloning and modification: {:?}", vec0);
 }
 
@@ -20,11 +19,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn move_semantics2() {
+    fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0.clone()); // Clone to retain ownership
-
-        assert_eq!(vec0, [22, 44, 66]);
-        assert_eq!(vec1, [22, 44, 66, 88]);
+        let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
