1bea34ab6e03452e442254ea9f3fda2d2d5a9056
linabenna <linabenna1@gmail.com>
2025-02-10 10:26:03 +0400

    Add files via upload

---
+++ 12_options/options1.rs
@@ -0,0 +1,42 @@
+// This function returns how much icecream there is left in the fridge.
+// If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
+// someone eats it all, so no icecream is left (value 0). Return `None` if
+// `hour_of_day` is higher than 23.
+fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
+    // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day < 22 {
+        Some(5)
+    } else {
+        Some(0)
+    }
+}
+
+fn main() {
+    println!("{}", maybe_icecream(4).unwrap_or(0)); // Prints 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn raw_value() {
+        // Extract the value from Option using `.unwrap()`
+        let icecreams = maybe_icecream(12).unwrap();
+        assert_eq!(icecreams, 5);
+    }
+
+    
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(0), Some(5));
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(18), Some(5));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(24), None);
+        assert_eq!(maybe_icecream(25), None);
+    }
+}
