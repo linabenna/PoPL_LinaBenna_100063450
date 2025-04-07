5a1505b78f7dd148c57d32e8e34788fe38be7fed
linabenna <linabenna1@gmail.com>
2025-04-07 09:25:19 +0400

    Add files via upload

---
+++ 18_iterators/iterators4.rs
@@ -0,0 +1,32 @@
+fn factorial(num: u64) -> u64 {
+    // computes the factorial of a number
+    (1..=num).product()
+}
+
+fn main() {
+    println!("Factorial of 4 is: {}", factorial(4));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(factorial(1), 1);
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(factorial(4), 24);
+    }
+}
